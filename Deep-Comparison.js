a5b575f2f55cdbfefa48b08f234720010a358990
Carl Miller <carlvmiller1@gmail.com>
2024-07-22 18:29:29 +0300

    Added return variable to recursion for loop in Deep-Comparison.js

--- Deep-Comparison.js
+++ Deep-Comparison.js
@@ -1,31 +1,27 @@
 function deepEqual(val1, val2) {
+    if (typeof val1 !== typeof val2) {
+        return false;
+    }
+
     if (val1 == null || val2 == null) {
         return val1 === val2;
     }
 
-    if (typeof val1 != "object" && typeof val2 !="object") {
+    if (typeof val1 !== "object" && typeof val2 !== "object" ) {
         return val1 === val2;
     }
 
-    function arraysAreEqual(array1, array2) {
-        if (array1.length !== array2.length) {
+    if (typeof val1 === "object" && typeof val2 === "object" ) {
+        if (Object.keys(val1).length !== Object.keys(val2).length) {
             return false;
         }
-        let result;
-        for (let i = 0; i < array1.length; i++) {
-            result = deepEqual(array1[i], array2[i]);
+        let valuesAreEqual = true;
+        for (let i = 0; i < Object.keys(val1).length && valuesAreEqual === true; i++) {
+            valuesAreEqual = deepEqual(Object.keys(val1)[i], Object.keys(val2)[i]);
+            if (valuesAreEqual === true) {
+                valuesAreEqual = deepEqual(Object.values(val1)[i], Object.values(val2)[i]);
+            }
         }
-        return result;
-    }
-    
-    const val1Keys = Object.keys(val1);
-    const val2Keys = Object.keys(val2);
-
-    if (arraysAreEqual(val1Keys, val2Keys)) {
-        const val1Objects = Object.values(val1);
-        const val2Objects = Object.values(val2);
-        return arraysAreEqual(val1Objects, val2Objects);
-    } else {
-        return false;
+        return valuesAreEqual;
     }
 }
